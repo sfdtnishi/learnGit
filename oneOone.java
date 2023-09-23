be86c422eb2cdaf7ea2d15db5d9b0853c76b4265
Nishi Jain <jain.nishi2105@gmail.com>
2023-09-23 18:30:57 +0530

    added one and two

---
+++ oneOone.java
@@ -0,0 +1,5 @@
+public class oneOone {
+    public static void main(String args[]){
+        System.out.println("work hard");
+    }
+}
---
+++ twoOtwo.java
@@ -0,0 +1,5 @@
+public class twoOtwo {
+    public static void main(String args[]){
+        System.out.println("work hard ma");
+    }
+}
